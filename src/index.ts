c7520e3c9f0a633caa8835ff15f6c45dfb65a73a
ranad4508 <rand4508@gmail.com>
2025-05-31 10:19:53 +0545

    Configuration update

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import session from "cookie-session";
+import { config } from "./config/app.config";
+import connectDatabase from "./config/database.config";
+
+import "./config/passport.config";
+import passport from "passport";
+
+const app = express();
+const BASE_PATH = config.BASE_PATH;
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    name: "session",
+    keys: [config.SESSION_SECRET],
+    maxAge: 24 * 60 * 60 * 1000,
+    secure: config.NODE_ENV === "production",
+    httpOnly: true,
+    sameSite: "lax",
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  cors({
+    origin: config.FRONTEND_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.listen(config.PORT, async () => {
+  console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
+  await connectDatabase();
+});
